15837047be321f72d43a6a2fc21babe63d26afa4
bardia-srj <bardia.sarjoughian27@gmail.com>
2024-07-18 16:24:12 +0330

    v3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,26 @@
+import { Link } from "react-router-dom";
 import Styled from "./Header.module.css"
-import { IoMenu } from "react-icons/io5";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaTelegram } from "react-icons/fa";
 const Header = () => {
     return (
         <>
             <div className={Styled.HeaderBox}>
-                <div className={Styled.LogoBox}>
-                    <img src="image/bettalogo.jpg" alt="" />
-                    
+                <div className={Styled.box1}>
+                    <img src="image/Untitled-1.png" alt="" />
                 </div>
-                <div className={Styled.Menu1}>
-                    <IoMenu />
+                <div className={Styled.box2}>
+                    <Link>برنامه ها</Link>
+                    <Link>قوانین و مقررات</Link>
+                    <Link>بلاگ</Link>
+                    <Link>تماس با ما</Link>
+                    <Link>درباره ما</Link>
                 </div>
-                {/* <div className={Styled.Menu2}></div> */}
+                <div className={Styled.box3}>
+
+                    <FaTelegram />
+                    <AiFillInstagram />
+                </div>
             </div>
         </>
     )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,7 @@ import Styled from "./Home.module.css"
 const Home = () =>{
     return(
         <>
-        <div className={Styled.HomeBox}>
-            <img src="image/Untitled-1.png" alt="" />
-            <span>سایت در حال بروزرسانی میباشد...</span>
-        </div>
-        
+        <Header />
         </>
     )
 }
